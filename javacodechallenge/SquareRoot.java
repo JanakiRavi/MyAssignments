716c8c77dee3bb74229a015d6989b2862d62dbdf
JanakiRavi <62099282+JanakiRavi@users.noreply.github.com>
2023-05-03 19:47:26 +0530

    Add files via upload

---
+++ javacodechallenge/SquareRoot.java
@@ -0,0 +1,13 @@
+package javacodechallenge;
+
+public class SquareRoot {
+
+	public static void main(String[] args) {
+		
+		int number=64;
+		int answer=(int) Math.sqrt(number);
+		
+		System.out.println(answer);
+	}
+
+}
