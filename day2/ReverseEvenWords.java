1d511ca50d8ba5459c7a14b891848cfdc4fe83dd
JanakiRavi <62099282+JanakiRavi@users.noreply.github.com>
2023-05-05 12:46:41 +0530

    Add files via upload

--- day2/ReverseEvenWords.java
+++ day2/ReverseEvenWords.java
@@ -1,24 +1,31 @@
-package week1.day2;
+package week2.day2;
 
 public class ReverseEvenWords {
 
 	public static void main(String[] args) {
 		
 		String test = "I am a software tester"; 
-		String[] words=test.split(" ");
-		String revString="";
-		String revwords="";
-		int i,j;
-		for(i=0;i<words.length-1;i++) {
-			
+	
+		String[] arr=test.split(" ");
+		String rev="";
+		
+		for(int i=0;i<arr.length;i++) {
 			if(i%2!=0) {
+	
+		char[] words=arr[i].toCharArray();
+		
+		for(int j=words.length-1;j>=0;j--) {
+			rev=rev+words[j];
+		}
+				System.out.print(rev+" ");
+			}
+			else {
+				System.out.print(arr[i]+" ");
+			}
 			
-			}
-			}
-	}
+		}
+
 		
 	}
 
-	
-
-
+}
